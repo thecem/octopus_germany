6c81ddea0139b274de176ab8ce7d4fdd4fea9642
thecem <46648579+thecem@users.noreply.github.com>
2025-04-22 16:17:41 +0200

    API Call Rate 2min

--- custom_components/octopus_germany/const.py
+++ custom_components/octopus_germany/const.py
@@ -5,4 +5,4 @@ DOMAIN = "octopus_germany"
 CONF_EMAIL = "email"
 CONF_PASSWORD = "password"
 
-UPDATE_INTERVAL = 3  # 3 minutes in minutes
+UPDATE_INTERVAL = 2  # 2 minutes The Octopus Energy APIs have a rate limit of 100 calls per hour, which is shared among all calls including through the app.
